0381e46054f9639a1619b3b2a0eab6815b45df8e
Christian Oleson <christian.oleson@imaginelearning.com>
2023-03-28 20:48:19 -0500

    Try break

---
+++ lint-action-test/lint-action-test/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace lint_action_test
+{
+    [TestClass]
+    public class UnitTest1 {
+        const string testItem = "TestItem";
+
+        [TestMethod]
+        public void TestMethod1() {
+            var MyTeStVar = 1;
+
+            int _myTeStVar2 = 2;
+
+            Assert.AreNotEqual(MyTeStVar, _myTeStVar2);
+
+            bool item1 = true; var item2 = false; 
+        }
+    }
+}
